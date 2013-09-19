9b2997e75fbb66190546687911f7bb4fe51fee90
stevengc777 <gracans777@yahoo.es>
2013-09-19 09:38:13 -0600

    add administrator pages

--- app/js/app.js
+++ app/js/app.js
@@ -7,10 +7,13 @@ var creditsTracking = angular.module('creditsTracking', ['firebase']);
 creditsTracking.value('fbWorkshops', 'https://credits-tracking.firebaseio.com/workshops');
 creditsTracking.value('fbMyWorkshops', 'https://credits-tracking.firebaseio.com/users');
 
+creditsTracking.value('fbAddWorkshops', 'https://mytracking.firebaseio.com/categories');
+
 creditsTracking.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
         .when('/page/workshops', {templateUrl: 'partials/workshops.html', controller: 'WorkshopsCtrl'})
         .when('/page/myworkshops', {templateUrl: 'partials/myworkshops.html', controller: 'MyWorkshopsCtrl'})
+        .when('/page/addworkshops', {templateUrl: 'partials/addworkshops.html', controller: 'AddWorkshopsCtrl'})
         .otherwise({templateUrl: 'partials/home.html'});
 }]);
 
@@ -24,4 +27,10 @@ creditsTracking.controller('WorkshopsCtrl', ['$scope', 'angularFireCollection', 
 
 creditsTracking.controller('MyWorkshopsCtrl', ['$scope', 'angularFireCollection', 'fbMyWorkshops', function ($scope, angularFireCollection, fbMyWorkshops) {
 	$scope.myworkshops = angularFireCollection(fbMyWorkshops);
+}]);
+
+
+
+creditsTracking.controller('AddWorkshopsCtrl', ['$scope', 'angularFireCollection', 'fbAddWorkshops', function ($scope, angularFireCollection, fbAddWorkshops) {
+	$scope.addworkshops = angularFireCollection(fbAddWorkshops);
 }]);
