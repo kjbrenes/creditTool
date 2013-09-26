a8703dde942e2266f186eeb5f763d21364cdec42
stevengc777 <gracans777@yahoo.es>
2013-09-26 15:18:52 -0600

    add functions for admin rol

--- app/js/app.js
+++ app/js/app.js
@@ -6,23 +6,25 @@ var creditsTracking = angular.module('creditsTracking', ['firebase']);
 
 creditsTracking.value('fbWorkshops', 'https://credits-tracking.firebaseio.com/workshops');
 creditsTracking.value('fbMyWorkshops', 'https://credits-tracking.firebaseio.com/users');
-creditsTracking.value('fbAddWorkshops', 'https://mytracking.firebaseio.com/categories');
+creditsTracking.value('fbAddWorkshops', 'hhttps://credits-tracking.firebaseio.com/categories');
 
-creditsTracking.value('user', 'user002');
+creditsTracking.value('user', 'user001');
 
 creditsTracking.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
         .when('/page/workshops', {templateUrl: 'partials/workshops.html'})
         .when('/page/myworkshops', {templateUrl: 'partials/myworkshops.html'})
-        .when('/page/addworkshops', {templateUrl: 'partials/addworkshops.html', controller: 'AddWorkshopsCtrl'})
+        .when('/page/addworkshops', {templateUrl: 'partials/addworkshops.html'})
+        .when('/page/pendingworkshops', {templateUrl: 'partials/pendingworkshops.html'})
         .otherwise({templateUrl: 'partials/home.html'});
 }]);
 /*---------------------------------------------------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------------------------------------------------*/
-creditsTracking.controller('CreditsTrackingCtrl', ['$scope', 'fbWorkshops', 'fbMyWorkshops', 'fbAddWorkshops', function ($scope, fbWorkshops, fbMyWorkshops, fbAddWorkshops) {
+creditsTracking.controller('CreditsTrackingCtrl', ['$scope', 'fbWorkshops', 'fbMyWorkshops', 'fbAddWorkshops', 'angularFireCollection', function ($scope, fbWorkshops, fbMyWorkshops, fbAddWorkshops, angularFireCollection) {
 	$scope.workshops = new Firebase(fbWorkshops);
 	$scope.myworkshops = new Firebase(fbMyWorkshops);
 	$scope.addworkshops = new Firebase(fbAddWorkshops);
+	$scope.saveWorkshop = angularFireCollection(new Firebase('https://credits-tracking.firebaseio.com/workshops'));
 
 }]);
 /*---------------------------------------------------------------------------------------------------------------------*/
@@ -79,9 +81,9 @@ creditsTracking.controller('WorkshopsCtrl', ['$scope', 'user', function ($scope,
 /*---------------------------------------------------------------------------------------------------------------------*/
 creditsTracking.controller('MyWorkshopsCtrl', ['$scope', 'user', function ($scope, user) {
 	var myworkshops = $scope.myworkshops, workshops = $scope.workshops, currentUser = myworkshops.child( user + '/workshops'), tempMyWorkshops = [];
-	
 	currentUser.on('child_added', function(snapshot) {
 		var myWsData = snapshot.val();
+		//console.log(myWsData);
 		workshops.on('child_added', function(snapshot) {
 			var wsData = snapshot.val();
 			if ((myWsData.id == wsData.id) && (!_.isUndefined(wsData.id))  ) {
@@ -100,6 +102,68 @@ creditsTracking.controller('MyWorkshopsCtrl', ['$scope', 'user', function ($scop
 }]);
 /*---------------------------------------------------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------------------------------------------------*/
-creditsTracking.controller('AddWorkshopsCtrl', ['$scope', function ($scope) {
-	
+creditsTracking.controller('PendingWorkshopsCtrl', ['$scope', function ($scope) {
+	var pendingworkshops = $scope.myworkshops, workshops = $scope.workshops, tempPendingWorkshops = [];
+
+	pendingworkshops.on('child_added', function(snapshot) {
+		var wsData = snapshot.val();
+	  	var allUsers = pendingworkshops.child( snapshot.name() + '/workshops');
+	  	allUsers.on('child_added', function(snapshot) {
+				var myWsData = snapshot.val();
+				workshops.on('child_added', function(snapshot) {
+					var wsDataWorkshop = snapshot.val();
+					if ((myWsData.id == wsDataWorkshop.id) && (myWsData.status == "pending") && (!_.isUndefined(myWsData.id))) {
+						tempPendingWorkshops.push({
+							id: wsDataWorkshop.id,
+							status: myWsData.status,
+							name: wsData.username, 
+							category: wsDataWorkshop.category
+						});
+					}
+				});
+		});
+	});
+	$scope.tempPendingWorkshops = tempPendingWorkshops;
+
+	$scope.changeState = function() {		
+		alert("si");
+    }
+
 }]);
+/*---------------------------------------------------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------------------------------------------------*/
+creditsTracking.controller('AddWorkshopsCtrl', ['$scope', function ($scope) {
+	var loadUsers = $scope.myworkshops, loadWorkshops = $scope.addworkshops, tempUsers = [], tempWorkshops = [];
+	loadUsers.on('child_added', function(snapshot) {
+		var wsData = snapshot.val();
+			tempUsers.push({
+				name: wsData.name + " " + wsData.firstname + " " + wsData.lastname
+			});
+	});
+	$scope.tempUsers = tempUsers;
+
+	loadWorkshops.on('child_added', function(snapshot) {
+		var myWsData = snapshot.val();
+		tempWorkshops.push({
+			credits: myWsData.credits,
+			category: myWsData.name
+		});
+	});
+	$scope.tempWorkshops = tempWorkshops;
+
+	$scope.selectedCategory = function() {
+    	loadWorkshops.on('child_added', function(snapshot) {
+		var myWsDataAll = snapshot.val();
+	    	if (($scope.myOption == myWsDataAll.name)  ) {
+				$scope.credits = myWsDataAll.credits;
+			}
+		});
+	};
+
+	$scope.addMessage = function() {
+		var currentDate = new Date();
+		$scope.saveWorkshop.add({author: $scope.myAuthorOption, category: $scope.myOption, creationdate: '12/02/2013', credits: $scope.credits, description: 'test', id: 'w0003', name: 'Test'});
+    }
+
+}]);
+/*---------------------------------------------------------------------------------------------------------------------*/
